a1a8329c4f6a9159aa368a1def16ce241df51594
kumarRajat77 <rajat.kumar07@hotmail.com>
2020-07-19 15:07:22 +0530

    Updates

---
+++ precache-manifest.2c05d4c01b138225408e8bbe2a507243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40013bc84698b0da687b9a996db2503",
+    "url": "/kittens-moserella/index.html"
+  },
+  {
+    "revision": "3e31a9a98e420da1c453",
+    "url": "/kittens-moserella/static/css/main.90ee0919.chunk.css"
+  },
+  {
+    "revision": "247d0a33b097cebed664",
+    "url": "/kittens-moserella/static/js/2.c706bae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittens-moserella/static/js/2.c706bae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e31a9a98e420da1c453",
+    "url": "/kittens-moserella/static/js/main.edd55256.chunk.js"
+  },
+  {
+    "revision": "0008987527ecf09887fa",
+    "url": "/kittens-moserella/static/js/runtime-main.85e51c87.js"
+  }
+]);
